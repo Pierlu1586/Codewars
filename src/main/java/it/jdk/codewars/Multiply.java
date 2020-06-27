df5799d0044b373ad4a4ce8c8ab1b41e681bf2b1
Pierlu1586 <you@example.com>
2020-06-27 07:39:54 +0200

    add kata Multiply

---
+++ src/main/java/it/jdk/codewars/Multiply.java
@@ -0,0 +1,23 @@
+//The code does not execute properly. Try to figure out why.
+
+package it.jdk.codewars;
+
+/**
+ *
+ * @author Pierluigi Mastroianni
+ */
+
+public class Multiply {
+    
+    public static void main(String[] args) {
+        double a = 2.3;
+        double b = 3.2;
+        
+        System.out.println(multiply(a,b));
+    }
+    
+    public static Double multiply(Double a, Double b){
+        return a * b;
+    }
+    
+}
